3301d73c4e46d6b7d16ae194276dcb87d6e17831
MCL7D9 <mcl7d9@outlook.com>
2022-10-24 20:10:38 +0800

    General

---
+++ Multi_99.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"99 Multi Table"<<"\n";
+    int x,y,z;
+    for (int i=0;i<10;i++){
+        for (int j=0;j<10;j++){
+            cout<<i<<"x"<<j<<"="<<i*j;
+            if ((i*j)<10)cout<<"  ";else cout<<" ";
+            
+        }
+        cout<<"\n";
+    }
+    system("pause");
+    return 0;
+}

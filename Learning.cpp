df203b853a722bd9f92cfa95d80a52680fb1b58d
MCL7D9 <mcl7d9@outlook.com>
2022-10-27 07:23:51 +0800

    General

---
+++ Learning.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+using namespace std;
+int cal(int,int);
+int main(){
+    int *ptr0,var=11;
+    ptr0=&var;
+    cout<<*ptr0;
+    return 0;
+}
+int cal(int x,int y){
+
+}
+

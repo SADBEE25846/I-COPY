2b4f4b6ad68269780874a5764a81525c7d453c6a
MCL7D9 <mcl7d9@outlook.com>
2022-10-27 17:50:51 +0800

    Test Functional Pointer

--- Learning.cpp
+++ Learning.cpp
@@ -2,10 +2,36 @@
 #include <cstdlib>
 #include <cmath>
 using namespace std;
-
+int sum(int,int);
+int sub(int,int);
+int mul(int,int);
 int main(){
-    int ls[5]={1,2,3,4,5};
-    cout<<*(ls+1);
+    int ls[5]={},x,y,(*ptr[])(int,int)={sum,sub,mul},mod;
+    cout<<"Selectc Mode"<<endl;
+    cin>>mod>>x>>y;
+    switch (mod)
+    {
+    case 0:
+        cout<<ptr[0](x,y);
+        break;
+    case 1:
+        cout<<ptr[1](x,y);
+        break;
+    case 2:
+        cout<<ptr[2](x,y);
+        break;
+    default:
+        break;
+    }
+
     return 0;
 }
-
+int sum(int a,int b){
+    return a+b;
+}
+int sub(int a,int b){
+    return a-b;
+}
+int mul(int a,int b){
+    return a*b;
+}

2e665a3d489e775583845c7cdb6c80b33487aed1
MCL7D9 <mcl7d9@outlook.com>
2022-10-27 19:05:27 +0800

    Add Progect Calculator

---
+++ Calculator.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+    int x,y,mod,(*ptr);
+    cout<<"Input Mode";
+    cin>>mod;
+    switch (mod)
+    {
+    case 0:
+        break;
+    
+    default:
+        break;
+    }
+    return 0;
+}
+int sum(){
+
+}
+int sub(){
+
+}
+int mul(){
+
+}

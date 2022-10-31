05885f615581992c42f6505b4068a0bcc68cbfb0
MCL7D9 <mcl7d9@outlook.com>
2022-10-31 12:39:30 +0800

    General

--- Test_Pointer.cpp
+++ Test_Pointer.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 #include <cstdlib>
+#include <string>
 using namespace std;
 void sum(int*,int*);
+int cal(int,int);
 int main(){
-    int var=10,var2=10,*ptr1=&var,*ptr2=&var2;
-    sum(ptr1,ptr2);
-    cout<<var;
-    return 0;
+    int var=10,var2=10,*ptr1=&var,*ptr2=&var2,(*ptr)(int,int);
+    struct Student{
+        string name;
+        int score;
+        char subject[4][10]={"Chinese","English","Math","Science"};
+    };
+    Student marco;
+    marco.score=100;
+    cout<<"After Break";
 }
-void sum(int *a,int *b){
-    *a=(*a)+(*b);
+int cal(int a,int b){
+    int sum;
+    return a+b;
 }
